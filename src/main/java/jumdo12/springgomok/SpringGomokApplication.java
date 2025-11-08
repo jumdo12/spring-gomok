9f42709d175d943b84d1394762a806cdb6ea4b7d
Han <jumdo12@gmail.com>
2025-11-09 01:06:46 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/jumdo12/springgomok/SpringGomokApplication.java
@@ -0,0 +1,13 @@
+package jumdo12.springgomok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGomokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGomokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jumdo12/springgomok/SpringGomokApplicationTests.java
@@ -0,0 +1,13 @@
+package jumdo12.springgomok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGomokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
